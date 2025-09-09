a41589c82efada7dd350cc9faecb987fcc230e2f
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-09-09 07:59:03 +0000

    Terminado

--- src/app.py
+++ src/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request 
 app = Flask(__name__)
-
+todos = [ { "label": "My first task", "done": False } ]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return "<h1>Hello!</h1>"
+    json_text = jsonify(todos)
+    return json_text
 
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json()
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    if position < 0 or position >= len(todos):
+        return jsonify({"error": "Índice fuera de rango"}), 400
+    todos.pop(position)
+    return jsonify(todos), 200
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
